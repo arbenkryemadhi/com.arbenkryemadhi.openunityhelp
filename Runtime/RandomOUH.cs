8f3b43b9f6807fcc95301474a05186784be8d5aa
Arben Kryemadhi <arbenofc@gmail.com>
2023-11-21 23:09:48 +0100

    New version

--- Runtime/RandomOUH.cs
+++ Runtime/RandomOUH.cs
@@ -149,7 +149,6 @@ namespace OpenUnityHelp
 			return UnityEngine.Random.Range(0, 100) + 1 <= percentage;
 		}
 
-		// Testttt
         /// <summary>
         /// Returns a bool with given probability in %. If probability is 25% it will return true each 4th time on an average.
         /// </summary>
